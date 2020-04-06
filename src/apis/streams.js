103a77ddd6aaf31a73f2cad1b800d21accd05265
gchapotot <guillaume.chapotot@meritis.fr>
2020-04-06 16:13:22 +0200

    add streaming with flv

--- src/apis/streams.js
+++ src/apis/streams.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+//Connexion to json-server api (local database)
 export default axios.create({
     baseURL: 'http://localhost:3001'
 });
--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -8,14 +8,10 @@ import {
 
 import _ from 'lodash';
 
-
-const INITIAL_STATE = {
-};
-
-export default (state = INITIAL_STATE, action) => {
+export default (state = {}, action) => {
     switch (action.type) {
         case FETCH_STREAMS:
-            return { ...state, ..._.mapKeys(action.payload, 'id')};
+            return { ...state, ..._.mapKeys(action.payload, 'id') };
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case CREATE_STREAM:
